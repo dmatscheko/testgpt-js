e0804d22647580b32fc9f7b0baa39a6005be0df4
David Matscheko <d.matscheko@analog-bit.com>
2023-03-29 20:30:42 +0200

    var rename

--- js/chatbox.js
+++ js/chatbox.js
@@ -75,31 +75,31 @@ class Chatbox {
     }
 
     // Formats one message as HTML
-    #formatMessage(messageObj, pos, msgIdx, msgCnt) {
+    #formatMessage(message, pos, msgIdx, msgCnt) {
         let type = 'ping';
-        if (messageObj.value.role === 'assistant') type = 'pong';
+        if (message.value.role === 'assistant') type = 'pong';
         const el = document.createElement('div');
         el.classList.add('message');
         el.classList.add(type);
         el.classList.add('hljs-nobg');
         el.classList.add('hljs-message');
-        if (messageObj.value.role === 'system') el.classList.add('system');
-        el.dataset.plaintext = encodeURIComponent(messageObj.value.content.trim());
+        if (message.value.role === 'system') el.classList.add('system');
+        el.dataset.plaintext = encodeURIComponent(message.value.content.trim());
         el.dataset.pos = pos;
 
         el.appendChild(this.#getAvatar(type));
 
-        let msgStat = ''
+        let msgStat = '';
         if (msgIdx > 0 || msgCnt > 1) msgStat = `<button title="Previous Message" class="msg_mod-prev-btn toolbtn small"><svg width="16" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 7.766c0-1.554-1.696-2.515-3.029-1.715l-7.056 4.234c-1.295.777-1.295 2.653 0 3.43l7.056 4.234c1.333.8 3.029-.16 3.029-1.715V7.766zM9.944 12L17 7.766v8.468L9.944 12zM6 6a1 1 0 0 1 1 1v10a1 1 0 1 1-2 0V7a1 1 0 0 1 1-1z" fill="currentColor"/></svg></button>&nbsp;${msgIdx + 1}/${msgCnt}&nbsp;<button title="Next Message" class="msg_mod-next-btn toolbtn small"><svg width="16" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 7.766c0-1.554 1.696-2.515 3.029-1.715l7.056 4.234c1.295.777 1.295 2.653 0 3.43L8.03 17.949c-1.333.8-3.029-.16-3.029-1.715V7.766zM14.056 12L7 7.766v8.468L14.056 12zM18 6a1 1 0 0 1 1 1v10a1 1 0 1 1-2 0V7a1 1 0 0 1 1-1z" fill="currentColor"/></svg></button>&nbsp;&nbsp;`;
         let model = '';
-        if (messageObj.metadata && messageObj.metadata.model) {
-            model = '&nbsp;<span class="right">' + messageObj.metadata.model + '</span>';
+        if (message.metadata && message.metadata.model) {
+            model = '&nbsp;<span class="right">' + message.metadata.model + '</span>';
         }
         const msgTitleStrip = document.createElement('small');
-        msgTitleStrip.innerHTML = `<span class="nobreak"><button title="New Message" class="msg_mod-add-btn toolbtn small"><svg width="16" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2h-6v6a1 1 0 1 1-2 0v-6H5a1 1 0 1 1 0-2h6V5a1 1 0 0 1 1-1z" fill="currentColor"/></svg></button>&nbsp;&nbsp;${msgStat}<b>${messageObj.value.role}</b>${model}</span><br><br>`;
+        msgTitleStrip.innerHTML = `<span class="nobreak"><button title="New Message" class="msg_mod-add-btn toolbtn small"><svg width="16" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2h-6v6a1 1 0 1 1-2 0v-6H5a1 1 0 1 1 0-2h6V5a1 1 0 0 1 1-1z" fill="currentColor"/></svg></button>&nbsp;&nbsp;${msgStat}<b>${message.value.role}</b>${model}</span><br><br>`;
         el.appendChild(msgTitleStrip);
 
-        const formattedEntities = this.#formatCodeBlocks(messageObj.value.content);
+        const formattedEntities = this.#formatCodeBlocks(message.value.content);
         if (formattedEntities) {
             el.appendChild(formattedEntities);
         } else {
