03c74a0917395260873243a35c39c0cafbdf3374
David Matscheko <d.matscheko@analog-bit.com>
2023-03-29 20:29:06 +0200

    a bit of refactoring

--- js/chatbox.js
+++ js/chatbox.js
@@ -83,69 +83,22 @@ class Chatbox {
         el.classList.add(type);
         el.classList.add('hljs-nobg');
         el.classList.add('hljs-message');
+        if (messageObj.value.role === 'system') el.classList.add('system');
         el.dataset.plaintext = encodeURIComponent(messageObj.value.content.trim());
+        el.dataset.pos = pos;
 
-        const avatar = document.createElement('img');
-        let avatarSrc = undefined;
-        let avatarFromLocalStorage = false;
-        try {
-            avatarSrc = localStorage.getItem(`${type}Avatar`);
-            avatarFromLocalStorage = avatarSrc !== null;
-        } catch (error) {
-            console.error(error);
-        }
-        avatar.src = avatarSrc || 'data:image/svg+xml,' + encodeURIComponent(type === 'ping' ? avatar_ping : avatar_pong);
-        avatar.addEventListener('click', () => {
-            if (avatarFromLocalStorage) {
-                const original = 'data:image/svg+xml,' + encodeURIComponent(type === 'ping' ? avatar_ping : avatar_pong);
-                avatar.src = original;
-                try {
-                    localStorage.removeItem(`${type}Avatar`);
-                } catch (error) {
-                    console.error(error);
-                }
-                this.chatlog.clearCache();
-                this.update(this.chatlog, false);
-                return;
-            }
-            const input = document.createElement('input');
-            input.type = 'file';
-            input.accept = 'image/*';
-            input.addEventListener('change', () => {
-                const file = input.files[0];
-                const reader = new FileReader();
-                reader.addEventListener('load', () => {
-                    try {
-                        localStorage.setItem(`${type}Avatar`, reader.result);
-                    } catch (error) {
-                        console.error(error);
-                    }
-                    avatar.src = reader.result;
-                    this.chatlog.clearCache();
-                    this.update(this.chatlog, false);
-                });
-                reader.readAsDataURL(file);
-            });
-            input.click();
-        });
-        avatar.style.width = '32px';
-        avatar.style.height = '32px';
-        avatar.style.objectFit = 'cover';
-        avatar.style.float = 'left';
-        avatar.style.marginRight = '8px';
-        el.appendChild(avatar);
+        el.appendChild(this.#getAvatar(type));
 
         let msgStat = ''
         if (msgIdx > 0 || msgCnt > 1) msgStat = `<button title="Previous Message" class="msg_mod-prev-btn toolbtn small"><svg width="16" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M19 7.766c0-1.554-1.696-2.515-3.029-1.715l-7.056 4.234c-1.295.777-1.295 2.653 0 3.43l7.056 4.234c1.333.8 3.029-.16 3.029-1.715V7.766zM9.944 12L17 7.766v8.468L9.944 12zM6 6a1 1 0 0 1 1 1v10a1 1 0 1 1-2 0V7a1 1 0 0 1 1-1z" fill="currentColor"/></svg></button>&nbsp;${msgIdx + 1}/${msgCnt}&nbsp;<button title="Next Message" class="msg_mod-next-btn toolbtn small"><svg width="16" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5 7.766c0-1.554 1.696-2.515 3.029-1.715l7.056 4.234c1.295.777 1.295 2.653 0 3.43L8.03 17.949c-1.333.8-3.029-.16-3.029-1.715V7.766zM14.056 12L7 7.766v8.468L14.056 12zM18 6a1 1 0 0 1 1 1v10a1 1 0 1 1-2 0V7a1 1 0 0 1 1-1z" fill="currentColor"/></svg></button>&nbsp;&nbsp;`;
-
         let model = '';
         if (messageObj.metadata && messageObj.metadata.model) {
             model = '&nbsp;<span class="right">' + messageObj.metadata.model + '</span>';
         }
-
         const msgTitleStrip = document.createElement('small');
         msgTitleStrip.innerHTML = `<span class="nobreak"><button title="New Message" class="msg_mod-add-btn toolbtn small"><svg width="16" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 4a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2h-6v6a1 1 0 1 1-2 0v-6H5a1 1 0 1 1 0-2h6V5a1 1 0 0 1 1-1z" fill="currentColor"/></svg></button>&nbsp;&nbsp;${msgStat}<b>${messageObj.value.role}</b>${model}</span><br><br>`;
         el.appendChild(msgTitleStrip);
+
         const formattedEntities = this.#formatCodeBlocks(messageObj.value.content);
         if (formattedEntities) {
             el.appendChild(formattedEntities);
@@ -154,9 +107,6 @@ class Chatbox {
             div.innerHTML = 'Error: Timeout on API server.';
             el.appendChild(div);
         }
-
-        if (messageObj.value.role === 'system') el.classList.add('system');
-        el.dataset.pos = pos;
 
         el.getElementsByClassName('msg_mod-add-btn')[0].addEventListener('click', async () => {
             const messageInp = document.getElementById("message-inp");
@@ -201,6 +151,56 @@ class Chatbox {
         return el;
     }
 
+    #getAvatar(type) {
+        const avatar = document.createElement('img');
+        let avatarSrc = undefined;
+        let avatarFromLocalStorage = false;
+        try {
+            avatarSrc = localStorage.getItem(`${type}Avatar`);
+            avatarFromLocalStorage = avatarSrc !== null;
+        } catch (error) {
+            console.error(error);
+        }
+        avatar.src = avatarSrc || 'data:image/svg+xml,' + encodeURIComponent(type === 'ping' ? avatar_ping : avatar_pong);
+        avatar.addEventListener('click', () => {
+            if (avatarFromLocalStorage) {
+                const original = 'data:image/svg+xml,' + encodeURIComponent(type === 'ping' ? avatar_ping : avatar_pong);
+                avatar.src = original;
+                try {
+                    localStorage.removeItem(`${type}Avatar`);
+                } catch (error) {
+                    console.error(error);
+                }
+                this.chatlog.clearCache();
+                this.update(this.chatlog, false);
+                return;
+            }
+            const input = document.createElement('input');
+            input.type = 'file';
+            input.accept = 'image/*';
+            input.addEventListener('change', () => {
+                const file = input.files[0];
+                const reader = new FileReader();
+                reader.addEventListener('load', () => {
+                    try {
+                        localStorage.setItem(`${type}Avatar`, reader.result);
+                    } catch (error) {
+                        console.error(error);
+                    }
+                    avatar.src = reader.result;
+                    this.chatlog.clearCache();
+                    this.update(this.chatlog, false);
+                });
+                reader.readAsDataURL(file);
+            });
+            input.click();
+        });
+        avatar.classList.add('avatar');
+
+        return avatar;
+    }
+
+    // TODO: also update SVG 
     #prepareTablesAndRemainingSvg(parent) {
         function tableToCSV(table) {
             const separator = ';';
